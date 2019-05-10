33d2b52e546e729a7ba672852c8daf52f5a44310
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2019-05-10 10:10:21 +0300

    JEN-957 install deps

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -144,7 +144,7 @@ pipeline {
                         fi
                         rm -f ${WORKSPACE}/VERSION-${BUILD_NUMBER}
                     '''
-                    git branch: '8.0', url: 'https://github.com/Percona-Lab/ps-build'
+                    git branch: 'jen-957_add_deps', url: 'https://github.com/hors/ps-build'
                     sh '''
                         # sudo is needed for better node recovery after compilation failure
                         # if building failed on compilation stage directory will have files owned by docker user
@@ -208,7 +208,7 @@ pipeline {
             steps {
                 timeout(time: pipeline_timeout, unit: 'HOURS')  {
                     retry(3) {
-                        git branch: '8.0', url: 'https://github.com/Percona-Lab/ps-build'
+                        git branch: 'jen-957_add_deps', url: 'https://github.com/hors/ps-build'
                         withCredentials([string(credentialsId: 'MTR_VAULT_TOKEN', variable: 'MTR_VAULT_TOKEN')]) {
                             sh '''
                                 sudo git reset --hard
